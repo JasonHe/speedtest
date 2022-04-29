a891e04ee4c6e95efc275d40c112d52b49de2c72
Jason He <59273498+JasonHe@users.noreply.github.com>
2022-04-29 23:54:15 +0800

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -89,8 +89,7 @@ checktar() {
 checkspeedtest() {
 	if  [ ! -e './speedtest-cli/speedtest' ]; then
 		echo "正在安装 Speedtest-cli"
-		wget --no-check-certificate -qO speedtest.tgz https://install.speedtest.net/app/cli/ookla-speedtest-1.0.0-$(uname -m)-linux.tgz
-		#wget --no-check-certificate -qO speedtest.tgz https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-$(uname -m)-linux.tgz 
+		wget --no-check-certificate -qO speedtest.tgz https://install.speedtest.net/app/cli/ookla-speedtest-1.1.1-linux-$(uname -m).tgz
 	fi
 	mkdir -p speedtest-cli && tar zxvf speedtest.tgz -C ./speedtest-cli/ > /dev/null 2>&1 && chmod a+rx ./speedtest-cli/speedtest
 }
@@ -416,8 +415,8 @@ runtest() {
 runall() {
 	checkroot;
 	checksystem;
-	checkpython;
-	checkcurl;
+#	checkpython;
+#	checkcurl;
 	checkwget;
 	checkspeedtest;
 	clear
